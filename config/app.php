3ca215e7420e13c03dc74e8eaba95c01b7042183
Hilmy Ghilmany <hilmyghilmani@gmail.com>
2023-04-04 12:54:13 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://hilmyghilmany.github.io/weeeb/'),
 
     'asset_url' => env('ASSET_URL', null),
 
